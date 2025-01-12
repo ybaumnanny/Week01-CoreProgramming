0133e3bf4e0402b8592d9e11f25309e98104b011
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-12 22:44:47 +0530

    Int Operation done

---
+++ Level-2 Practice Programs/IntOperation.java
@@ -0,0 +1,26 @@
+/*Write an IntOperation program by taking a, b, and c as input values and print the following integer operations a + b *c, a * b + c, c + a / b, and a % b + c. Please also understand the precedence of the operators. 
+Hint => 
+Create variables a, b, c of int data type.
+Take user input for a, b, and c.
+Compute 3 integer operations and assign result to a variable
+Finally print the result and try to understand operator precedence.
+I/P => fee, discountPrecent
+O/P => The results of Int Operations are —-, -—, and —-*/
+
+import java.util.Scanner;
+
+public class IntOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int result1 = a + b * c;
+        int result2 = a * b + c;
+        int result3 = c + a / b;
+        int result4 = a % b + c;
+
+        System.out.println("The results of Int Operations are " + result1 + ", " + result2 + ", " + result3 + ", " + result4);
+    }
+}
