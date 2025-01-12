a988a1982cd4ebd800e77c85e85932f7bbef9272
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-12 23:48:23 +0530

    Multiplication Table

---
+++ Level-1 Control Flow/MultiplicationTable.java
@@ -0,0 +1,19 @@
+/*Create a program to find the multiplication table of a number entered by the user from 6 to 9.
+Hint => 
+Take integer input and store it in the variable number
+Using a for loop, find the multiplication table of number from 6 to 9 and print it in the format number * i = ___ 
+*/
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 6; i <= 9; i++) { //Using a for loop for the multiplication table
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
+
