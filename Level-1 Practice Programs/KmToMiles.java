9a898e9f67295b079f5d9f7ed16ceba126b49501
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-12 22:19:52 +0530

    Km To Miles done

---
+++ Level-1 Practice Programs/KmToMiles.java
@@ -0,0 +1,16 @@
+/*Create a program to convert the distance of 10.8 kilometers to miles.
+Hint: 1 km = 1.6 miles
+I/P => NONE
+O/P => The distance  ___ km in miles is ___
+*/
+public class KmToMiles{
+	public static void main(String[]args){
+		 double kilometers = 1.6;
+		 double miles = kilometers * 1.6;
+		 //1 km = 1.6 miles
+		 
+		 
+		 System.out.println("The distance " +  kilometers + "km in  miles is " + miles);
+	}
+   
+}
