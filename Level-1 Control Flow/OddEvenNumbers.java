2f0f963d0a894bef33f89c945e4767a6f3c2189c
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-12 23:48:58 +0530

    Odd Even Numbers

---
+++ Level-1 Control Flow/OddEvenNumbers.java
@@ -0,0 +1,28 @@
+/*Create a program to print odd and even numbers between 1 to the number entered by the user.
+Hint => 
+Get an integer input from the user, assign to a variable number and check for Natural Number
+Using a for loop, iterate from 1 to the number
+In each iteration of the loop, print the number is odd or even number
+*/
+
+import java.util.Scanner;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even");
+                } else {
+                    System.out.println(i + " is odd");
+                }
+            }
+        } else {
+            System.out.println("The number is not a positive integer.");
+        }
+    }
+}
