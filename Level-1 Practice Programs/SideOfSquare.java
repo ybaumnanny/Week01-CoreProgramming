f8522e7366d7d8fdbab928d57dc12fdbef9f8895
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-12 22:32:01 +0530

    Side Of Square

---
+++ Level-1 Practice Programs/SideOfSquare.java
@@ -0,0 +1,16 @@
+/*Write a program to find the side of the square whose parameter you read from user 
+Hint => Perimeter of Square is 4 times side
+I/P => perimeter
+O/P => The length of the side is ___ whose perimeter is ____
+*/
+
+import java.util.*;
+
+public class SideOfSquare {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double perimeter = input.nextDouble();
+        double side = perimeter / 4;
+        System.out.println("The length of the side is" +side+  "whose perimeter is"+perimeter);
+    }
+}
