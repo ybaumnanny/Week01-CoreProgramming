4890df87a75d179ef89f5630444aed779c4da2d8
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-13 10:50:31 +0530

    Abundant Number

---
+++ Level-3 Control Flow/AbundantNumber.java
@@ -0,0 +1,37 @@
+/*Create a program to check if a number is an Abundant Number.
+Hint => 
+An abundant number is an integer in which the sum of all the divisors of the number is greater than the number itself. For example,
+Divisor of 12: 1, 2, 3, 4, 6
+Sum of divisor: 1 + 2 + 3 + 4 + 6 = 16 > 12
+Get an integer input for the number variable.
+Create an integer variable sum with initial value 0.
+Run a for loop from i = 1 to i < number.
+Inside the loop, check if number is divisible by i.
+If true, add i to sum.
+Outside the loop Check if sum is greater than number.
+If the sum is greater than the number, print Abundant Number. Otherwise, print Not an Abundant Number.
+*/
+
+
+import java.util.Scanner;
+
+public class AbundantNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;  // Add divisor to sum
+            }
+        }
+        
+        // Check if the number is abundant
+        if (sum > number) {
+            System.out.println(number + " is an Abundant number.");
+        } else {
+            System.out.println(number + " is not an Abundant number.");
+        }
+    }
+}
