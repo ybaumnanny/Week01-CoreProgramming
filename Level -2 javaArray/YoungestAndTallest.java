90ce5f0ec982ddf2e640fef0798ef89cc6c055c3
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-14 14:56:12 +0530

    Youngest And Tallest

---
+++ Level -2 javaArray/YoungestAndTallest.java
@@ -0,0 +1,43 @@
+/*Create a program to find the youngest friends among 3 Amar, Akbar, and Anthony based on their ages and the tallest among the friends based on their heights
+Hint => 
+Take user input for age and height for the 3 friends and store it in two arrays each to store the values for age and height of the 3 friends
+Loop through the array and find the youngest of the 3 friends and the tallest of the 3 friends
+Finally display the youngest and tallest of the 3 friends
+*/
+
+import java.util.Scanner;
+
+public class YoungestAndTallest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] friends = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        int[] heights = new int[3];
+
+        // Taking input for ages and heights
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + friends[i] + ": ");
+            ages[i] = scanner.nextInt();
+            System.out.print("Enter height of " + friends[i] + " in cm: ");
+            heights[i] = scanner.nextInt();
+        }
+
+        // Finding the youngest and the tallest
+        int youngestIndex = 0;
+        int tallestIndex = 0;
+
+        for (int i = 1; i < 3; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        }
+
+        // Displaying the results
+        System.out.println("The youngest friend is " + friends[youngestIndex]);
+        System.out.println("The tallest friend is " + friends[tallestIndex]);
+    }
+}
