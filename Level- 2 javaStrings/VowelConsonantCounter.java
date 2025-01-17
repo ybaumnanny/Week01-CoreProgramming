05c8a5fc44fe161b3e81c502ca99e7698182a100
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-17 12:48:58 +0530

    Vowel Consonant Counter done

---
+++ Level- 2 javaStrings/VowelConsonantCounter.java
@@ -0,0 +1,53 @@
+/*
+Write a program to find vowels and consonants in a string and display the count of  Vowels and Consonants in the string
+Hint => 
+Create a method to check if the character is a vowel or consonant and return the result. The logic used here is as follows:
+Convert the character to lowercase if it is an uppercase letter using the ASCII values of the characters
+Check if the character is a vowel or consonant and return Vowel, Consonant, or Not a Letter
+Create a Method to Method to find vowels and consonants in a string using charAt() method and finally return the count of vowels and consonants in an array
+Finally, the main function takes user inputs, calls the user-defined methods, and displays the result. 
+*/
+import java.util.Scanner;
+public class VowelConsonantCounter {
+    public static String checkCharacter(char ch) {
+        ch = Character.toLowerCase(ch);
+        if ((ch >= 'a' && ch <= 'z')) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return "Vowel";
+            } else {
+                return "Consonant";
+            }
+        }
+        return "Not a Letter";
+    }
+
+    // Method to find the no of vowels and consonants 
+    public static int[] countVowelsAndConsonants(String text) {
+        int vowelCount = 0;
+        int consonantCount = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            String result = checkCharacter(text.charAt(i));
+            if (result.equals("Vowel")) {
+                vowelCount++;
+            } else if (result.equals("Consonant")) {
+                consonantCount++;
+            }
+        }
+
+        return new int[]{vowelCount, consonantCount};
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+
+        int[] counts = countVowelsAndConsonants(input);
+        System.out.println("Number of Vowels: " + counts[0]);
+        System.out.println("Number of Consonants: " + counts[1]);
+
+        scanner.close();
+    }
+}
