8f2d155eeaf4f684da7cf28898240ba000f89ca5
Yaman Mahtha <yamanmahtha@gmail.com>
2025-01-17 11:38:03 +0530

    Number Checker Second

---
+++ Level - 3 javaMethods/NumberCheckerSecond.java
@@ -0,0 +1,90 @@
+/*
+Extend or Create a NumberChecker utility class and perform following task. Call from main() method the different methods and display results. Make sure all are static methods
+Hint => 
+Method to find the count of digits in the number and a Method to Store the digits of the number in a digits array
+Method to find the sum of the digits of a number using the digits array
+Method to find the sum of the squares of the digits of a number using the digits array. Use Math.pow() method
+Method to Check if a number is a harshad number using a digits array. A number is called a Harshad number if it is divisible by the sum of its digits. For e.g. 21
+Method to find the frequency of each digit in the number. Create a 2D array to store the frequency with digit in the first column and frequency in the second column.
+*/
+
+public class NumberCheckerSecond {
+
+    // Method to find the count of digits in the number
+    public static int countDigits(int number) {
+        return String.valueOf(Math.abs(number)).length();
+    }
+
+    // Method to store the digits of the number in a digits array
+    public static int[] getDigitsArray(int number) {
+        String numStr = String.valueOf(Math.abs(number));
+        int[] digits = new int[numStr.length()];
+        for (int i = 0; i < numStr.length(); i++) {
+            digits[i] = Character.getNumericValue(numStr.charAt(i));
+        }
+        return digits;
+    }
+
+    // Method to find the sum of the digits of a number using the digits array
+    public static int sumOfDigits(int[] digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            sum += digit;
+        }
+        return sum;
+    }
+
+    // Method to find the sum of the squares of the digits of a number using the digits array
+    public static int sumOfSquaresOfDigits(int[] digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            sum += Math.pow(digit, 2);
+        }
+        return sum;
+    }
+
+    // Method to check if a number is a Harshad number using a digits array
+    public static boolean isHarshadNumber(int number) {
+        int[] digits = getDigitsArray(number);
+        int sum = sumOfDigits(digits);
+        return number % sum == 0;
+    }
+
+    // Method to find the frequency of each digit in the number
+    public static int[][] digitFrequency(int number) {
+        int[] digits = getDigitsArray(number);
+        int[][] frequency = new int[10][2];
+        for (int i = 0; i < 10; i++) {
+            frequency[i][0] = i;
+        }
+        for (int digit : digits) {
+            frequency[digit][1]++;
+        }
+        return frequency;
+    }
+
+    public static void main(String[] args) {
+        int number = 12345;
+
+        System.out.println("Count of digits: " + countDigits(number));
+
+        int[] digitsArray = getDigitsArray(number);
+        System.out.print("Digits array: ");
+        for (int digit : digitsArray) {
+            System.out.print(digit + " ");
+        }
+        System.out.println();
+
+        System.out.println("Sum of digits: " + sumOfDigits(digitsArray));
+        System.out.println("Sum of squares of digits: " + sumOfSquaresOfDigits(digitsArray));
+        System.out.println("Is Harshad number: " + isHarshadNumber(number));
+
+        int[][] frequency = digitFrequency(number);
+        System.out.println("Digit frequency:");
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i][1] > 0) {
+                System.out.println("Digit " + frequency[i][0] + ": " + frequency[i][1] + " times");
+            }
+        }
+    }
+}
